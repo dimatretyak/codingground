e3251b6a696b6ce0689629ce470bc00e8b8e96c7
Dima Tretyak <itretyak@ya.ru>
2016-09-29 02:23:46 +0300

    Update main.cpp

--- New Project-20160928/main.cpp
+++ New Project-20160928/main.cpp
@@ -5,12 +5,15 @@
 using namespace std;
 
 int main() {
-	double a, b, c, d, e, result;
-	//puts("\n\t x, y, z = ");		
-	//scanf("%lf %lf %lf", &x, &y, &z);
-	double x = 3.74*pow(10,-2); //0.0374
-	double y = -0.825; //-0.825
-	double z = 0.16*pow(10,2); //16
+	double x, y, z, a, b, c, d, e, result;
+	puts("\n\t x, y, z = ");		
+	scanf("%lf %lf %lf", &x, &y, &z);
+	
+	/* Тестовые значения для проверки ответа
+	x = 3.74*pow(10,-2); //0.0374
+	y = -0.825; //-0.825
+	z = 0.16*pow(10,2); //16
+	*/
 	
 	// Вычисления
 	a = 1 + pow(sin(x+y), 2);
@@ -18,7 +21,7 @@ int main() {
 	c = a/b*pow(x,fabs(y));
 	d = atan2(1,z);
 	e = pow(cos(d), 2);
-	//result = c + e;
+	result = c + e;
 	//result = (((1 + pow(sin(x+y), 2)) / fabs(x - (2*y/1 + (pow(x,2)*pow(y,2))))) * pow(x,fabs(y))) + (pow(cos(atan2(1,z)), 2));
 	
 	cout << result << '\n';
